4af6f901c584120e8f25c44266aa88c8afc61a1a
Faruk <90871155+Faruk8502@users.noreply.github.com>
2023-07-05 21:27:16 +0300

    Add files via upload

---
+++ tests/Test_2.py
@@ -0,0 +1,16 @@
+import pickle
+import numpy as np
+from Stationary_Diagnosis import Stationary_Diagnosis
+
+with open('med_recs_depers.pkl', 'rb') as f:
+    x = pickle.load(f)
+# d = x.loc[x.Статус == 'ДИАГНОЗ СТАЦИОНАРНЫЙ', 'Данные']
+d = x.loc[x.Статус == 'ЖАЛОБЫ', 'Данные']
+path_dictionary = "dictionary.csv"
+if __name__ == '__main__':
+    for i in range(0, np.size(d)):
+        try:
+            Object = Stationary_Diagnosis(d.iloc[i], path_dictionary)
+            print('________________________________________________')
+        except:
+            i = i+1
