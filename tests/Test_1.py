8127fff80f1a1eb13831cdd0828a64e9b10183f3
Faruk <90871155+Faruk8502@users.noreply.github.com>
2023-07-08 11:44:35 +0300

    Update Test_1.py

--- tests/Test_1.py
+++ tests/Test_1.py
@@ -1,16 +1,21 @@
 import pickle
 import numpy as np
-from Stationary_Diagnosis import Stationary_Diagnosis
+import sys
+import os
 
-with open('med_recs_depers.pkl', 'rb') as f:
+DIR = os.path.dirname(os.path.dirname(__file__))
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from parsers.Stationary_Diagnosis import Stationary_Diagnosis as parser
+with open(f'{DIR}/recs/med_recs_depers.pkl', 'rb') as f:
     x = pickle.load(f)
 d = x.loc[x.Статус == 'ДИАГНОЗ СТАЦИОНАРНЫЙ', 'Данные']
-# d = x.loc[x.Статус == 'ЖАЛОБЫ', 'Данные']
-path_dictionary = "dictionary.csv"
-if __name__ == '__main__':
-    for i in range(0, np.size(d)):
+path_dictionary = f'{DIR}/terms/dictionary.csv'
+print(path_dictionary)
+for i in range(0, np.size(d)):
         try:
-            Object = Stationary_Diagnosis(d.iloc[i], path_dictionary)
+            Object = parser(d.iloc[i], path_dictionary)
+            print(Object)
             print('________________________________________________')
         except:
             i = i+1
